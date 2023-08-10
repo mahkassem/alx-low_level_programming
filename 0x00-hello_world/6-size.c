30c22598ea816b0d8f12f110509433a93ecd4737
mahka <mahmoudalikassem@outlook.com>
2023-08-10 22:56:24 +0300

    fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,12 +8,13 @@
 int main(void)
 {
 /**
- * Write a C program that prints the size of various types on the computer it is compiled and run on.
+ * Main function:
  *
  * - You should produce the exact same output as in the example
  * - Warnings are allowed
  * - Your program should return 0
- * - You might have to install the package libc6-dev-i386 on your Linux to test the -m32 gcc option
+ * - You might have to install the package libc6-dev-i386 on
+ * your Linux to test the -m32 gcc option
  */
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
 printf("Size of an int: %lu byte(s)\n", sizeof(int));
