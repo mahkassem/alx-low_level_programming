52ed076f343012d7deaf5e67244c8d9bdc643a47
mahka <mahmoudalikassem@outlook.com>
2023-08-10 22:45:14 +0300

    attempt fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,6 @@
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle"); // puts() function prints a string to stdout
 return (0);
-}
+} // End main
