7dec05081fa12b12ef53f31945f72c2813293afa
mahka <mahmoudalikassem@outlook.com>
2023-08-10 22:58:14 +0300

    fix 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,19 +7,15 @@
  */
 int main(void)
 {
-/**
- * Main function:
- *
- * - You should produce the exact same output as in the example
- * - Warnings are allowed
- * - Your program should return 0
- * - You might have to install the package libc6-dev-i386 on
- * your Linux to test the -m32 gcc option
- */
+/** char size */
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
+/** int size */
 printf("Size of an int: %lu byte(s)\n", sizeof(int));
+/** long int size */
 printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+/** long long int size */
 printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+/** float size */
 printf("Size of a float: %lu byte(s)\n", sizeof(float));
 return (0);
 } /** End main */
