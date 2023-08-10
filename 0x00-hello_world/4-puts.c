e1d7daa22dc346d6dbb395004e3f9ab3a3ddb840
mahka <mahmoudalikassem@outlook.com>
2023-08-10 22:40:05 +0300

    fix 3

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /*
-* Main function returns 0 
-*/
+ * Main function returns 0
+ */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
-return (0);
+  puts("\"Programming is like building a multilingual puzzle\n");
+  return (0);
 }
