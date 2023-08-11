11daf7ede9836308dff666a101590190e4ed8944
mahka <mahmoudalikassem@outlook.com>
2023-08-11 18:56:46 +0300

    print a to z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	char c;
+
+	/** print a to z*/
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	/** return 0 */
+	return (0);
+}
