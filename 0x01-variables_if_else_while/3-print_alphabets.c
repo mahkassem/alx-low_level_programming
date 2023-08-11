3238ce76a761b66fa19a33a0118199799ea358b7
mahka <mahmoudalikassem@outlook.com>
2023-08-11 18:58:57 +0300

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	char c;
+
+	/** print a to z + A to Z*/
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+
+	/** return 0 */
+	return (0);
+}
