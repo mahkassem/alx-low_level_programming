a02c35c5972967fecda37e4c7e273dcad59992cd
mahka <mahmoudalikassem@outlook.com>
2023-08-24 02:04:04 +0300

    0x06

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates  strings
+ * @dest: char
+ * @src: char
+ *
+ * Return: char *
+ */
+char *_strcat(char *dest, char *src)
+{
+
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse an array
+ * @a: int *
+ * @n: int
+ */
+void reverse_array(int *a, int n)
+{
+	int tmp, index;
+
+	for (index = n - 1; index > n / 2; index--)
+	{
+		tmp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
+		a[index] = tmp;
+	}
+}
