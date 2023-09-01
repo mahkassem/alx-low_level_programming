bad3a061e3bc8d095ab2f0f59dbba93cf9e065f1
mahka <mahmoudalikassem@outlook.com>
2023-09-01 18:08:35 +0300

    add 0x0A

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print number of arguments
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: int
+ */
+int main(int argc, char __attribute__((__unused__)) * argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print sum of positive arguments
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
